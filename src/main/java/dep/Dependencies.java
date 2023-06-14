f0f2c4f5d9732babdd6d4c0ac2ca2e1adefd9d65
Olga Lavtar <olavtar@redhat.com>
2023-06-14 13:02:05 -0700

    added jakarta

--- src/main/java/dep/Dependencies.java
+++ src/main/java/dep/Dependencies.java
@@ -2,14 +2,21 @@ package dep;
 
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Invocation;
 import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 public class Dependencies {
-    private String test;
 
-    Client client = ClientBuilder.newClient();
-    WebTarget target = client.target("http://example.com/base/uri");
-    Response response = target.request().get();
-    String value = response.readEntity(String.class);
+    public static void main(String[] args) {
+
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("https://jsonplaceholder.typicode.com/todos/1");
+        Invocation.Builder builder = target.request(MediaType.APPLICATION_JSON_TYPE);
+        Response response = builder.get();
+        String value = response.readEntity(String.class);
+        System.out.println("Value: " + value);
+    }
+
 }
